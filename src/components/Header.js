e11392af4903311aa62b06b4e3a04d6f8f319b1e
Unique-coder <tom-lesh@outlook.com>
2022-01-26 18:41:56 +0100

    UseLocator: use route to change rendered components. change rendering without refreshing screen.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import Button from "./Button";
+// Look at the route(link) we are currently on and make changes to the rendered page based on it.
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAddTask ? "red" : "green"}
-        text={showAddTask ? "close" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAddTask ? "red" : "green"}
+          text={showAddTask ? "close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
       {/* <Button color="red" text="Remove" /> */}
     </header>
   );
